fdae9756892e5b57871286dd6398ed1f1f4da1ad
jwagner31 <70210002+jwagner31@users.noreply.github.com>
2022-05-02 19:24:21 +0000

    working on alignment

--- main.py
+++ main.py
@@ -52,11 +52,7 @@ def fast_MED(S, T):
 
 
 def fast_align_MED(S, T, MED={}):
-  S_align = ""
-  T_align = ""
-    memo = [[0 for col in range(len(T)+1)] for row in range(len(S)+1)]
-  # memo[1:][0] = range(1, len(S)+1)
-  # memo[0][1:] = range(1, len(T)+1)
+  memo = [[0 for col in range(len(T)+1)] for row in range(len(S)+1)]
   for i in range(len(S) + 1):
     for j in range(len(T) +1):
       if i==0:
@@ -67,15 +63,38 @@ def fast_align_MED(S, T, MED={}):
         memo[i][j] = memo[i-1][j-1]
       else:
         memo[i][j] = 1 + min(memo[i][j-1], memo[i-1][j], memo[i-1][j-1])
-  i = 0
-  j = 0
-  while(i <= len(S) or j <= len(T)):
-    if i==len(S) and j==len(T):
-      return S_align, T_align
-    if 
-  #right (i+1, j) = gap for S and add letter to T
-  #left (i, j+1) = letter for S and gap for T
-  #diagonal = letter for both
+  S_align = ""
+  T_align = ""
+  alignloop = True
+  x = len(S)
+  y = len(T)
+  print(memo)
+  while(alignloop):
+    curr = min(memo[x-1][y], memo[x][y-1], memo[x-1][y-1])
+    #Diagonal --> Either substitution or no sub
+    if curr == memo[x-1][y-1]:
+      S_align = S[x-1] + S_align
+      T_align = T[y-1] + T_align
+      x-=1
+      y-=1
+    #Up or Down if Not
+    elif curr == memo[x-1][y]:
+      S_align = S[x-1] + S_align
+      T_align = "-" + T_align
+      x-=1
+    elif curr == memo[x][y-1]:
+      S_align = "-" + S_align
+      T_align = T[y-1] + T_align
+      y-=1
+    print("S: "+ S_align+" T: "+T_align)
+    #Reached bottom left of memoization table
+    if x==0 or y==0:
+      alignloop = False
+      break
+  return S_align, T_align
+    
+    
+  
   
 
 def test_MED():
